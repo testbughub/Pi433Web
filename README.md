31694b792a18b19c75679e5397d47ff7e69a5c4e
testbughub <andreas.lamarca@gmail.com>
2020-10-22 11:26:44 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,48 @@
+## Description
+Originally forked from [SmartPi](https://github.com/RonnyFalconeri/SmartPi),  
+but because it was a while since it was updated, the module used for  
+the 433MHz operations has become deprecated.  
+So I decided to throw together my own repo, but with the updated module.  
+I also made minor additions and changes, for example, I changed the webserver  
+port from **8080** to **8043** because I use 8080 for another webserver.
+
+(Below is a copy of [SmartPi's](https://github.com/RonnyFalconeri/SmartPi) README.  
+
+## Software
+This is a project made with **Node.js** so you will need it installed on your Pi.
+
+
+### How to install WiringPi:
+
+To get the application running you will first need to install WiringPi. 
+
+More infos: https://projects.drogon.net/raspberry-pi/wiringpi/
+
+``` $ git clone git://git.drogon.net/wiringPi ```
+
+``` $ cd wiringPi/wiringPi ```
+
+``` $ sudo su ```
+
+``` $ ./build ```
+
+### Getting started
+
+To clone this repository just simply run:
+
+``` $ git clone https://github.com/RonnyFalconeri/SmartPi.git ```
+  
+To run the webserver open the SmartPi directory and run:
+
+``` $ node webserver.js ```
+
+I recommend to set up a startscript which runs the server automaticly after booting. [Here](https://www.instructables.com/id/Nodejs-App-As-a-RPI-Service-boot-at-Startup/ "Title") is a link to a guide on how to do it.
+
+Your wireless receiver you are looking forward to control may have different codes to be turned on. In this case you will need to sniff the code your remote control is sending in order to send it again with the Pi. [Here](https://www.princetronics.com/how-to-read-433-mhz-codes-w-raspberry-pi-433-mhz-receiver/ "Title") is a link on how to do it, **or** just usw my other project 'RF_Sniffer' which is an application just for sniffing 433MHz signals.
+
+## Hardware
+You don't necessarily need a Raspberry Pi for this but I recommend it because you will always find help on the internet by its big community.
+You also need a 433MHz emitter. The data pin is wired to the physical pin 11 of the Pi. You can change this of course but make sure to also change the pin allocation in the code.
+
+
+Have fun with the project and don't hesitate to improve it ;)
---
+++ webserver.js
@@ -0,0 +1,200 @@
+// required modules
+var http = require('http').createServer(handler);
+var fs = require('fs');
+var io = require('socket.io')(http)
+
+// required module for sending code trough pins and via RF Module
+var rpi433 = require('rpi-433-v3'),
+  rfSniffer = rpi433.sniffer({
+    pin: 2,                     //Sniff on GPIO 2 (or Physical PIN 13)
+    debounceDelay: 500          //Wait 500ms before reading another code
+  }),
+  rfEmitter = rpi433.emitter({
+    pin: 0,                     //Send through GPIO 0 (or Physical PIN 11)
+    pulseLength: 322            //Send the code with a 350 pulse length
+  });
+
+http.listen(8043);  // Server listens to port 8080
+
+// Handles request from client, sends index.html
+function handler(req, res) {
+  fs.readFile(__dirname + '/index.html', function (err, data) {
+    if (err) {
+      res.writeHead(404, { 'Content-Type': 'text/html' });
+      return res.end("404 Not Found");
+    }
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    res.write(data);
+    return res.end();
+  });
+}
+
+// object containing on and off codes from it's receivers
+// the codes were sniffed with the rfSniffer module at the top by pressing the buttons on the remote
+var sockets = {
+  a: {
+    on: '1115473',
+    off: '1115476'
+  },
+  b: {
+    on: '1118545',
+    off: '1118548'
+  },
+  c: {
+    on: '1119313',
+    off: '1119316'
+  },
+  d: {
+    on: '1119505',
+    off: '1119508'
+  },
+  e: {
+    on: '1119553',
+    off: '1119556'
+  },
+  f: {
+    on: '1131857',
+    off: '1131860'
+  }
+};
+
+// Astablishes a socket connection with client
+io.sockets.on('connection', function (socket) {
+
+  var switcher = false;
+
+  // A receiver -----------------------------------------------------
+  socket.on('a_switcher', function (data) {
+
+    switcher = data;
+
+    if (switcher) {
+
+      // if true, turn on 
+      rfEmitter.sendCode(sockets.a.on, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    } else {
+
+      // if false, turn off 
+      rfEmitter.sendCode(sockets.a.off, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    }
+  });
+
+
+
+  // B receiver -----------------------------------------------------
+  socket.on('b_switcher', function (data) {
+
+    switcher = data;
+
+    if (switcher) {
+
+      // if true, turn on 
+      rfEmitter.sendCode(sockets.b.on, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    } else {
+
+      // if false, turn off 
+      rfEmitter.sendCode(sockets.b.off, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    }
+  });
+
+
+  // C receiver -----------------------------------------------------
+  socket.on('c_switcher', function (data) {
+
+    switcher = data;
+
+    if (switcher) {
+
+      // if true, turn on 
+      rfEmitter.sendCode(sockets.c.on, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    } else {
+
+      // if false, turn off 
+      rfEmitter.sendCode(sockets.c.off, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    }
+  });
+
+  // D receiver -----------------------------------------------------
+  socket.on('d_switcher', function (data) {
+
+    switcher = data;
+
+    if (switcher) {
+
+      // if true, turn on 
+      rfEmitter.sendCode(sockets.d.on, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    } else {
+
+      // if false, turn off 
+      rfEmitter.sendCode(sockets.d.off, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    }
+  });
+
+  // E receiver -----------------------------------------------------
+  socket.on('e_switcher', function (data) {
+
+    switcher = data;
+
+    if (switcher) {
+
+      // if true, turn on 
+      rfEmitter.sendCode(sockets.e.on, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    } else {
+
+      // if false, turn off 
+      rfEmitter.sendCode(sockets.e.off, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    }
+  });
+
+  // F receiver -----------------------------------------------------
+  socket.on('f_switcher', function (data) {
+
+    switcher = data;
+
+    if (switcher) {
+
+      // if true, turn on 
+      rfEmitter.sendCode(sockets.f.on, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    } else {
+
+      // if false, turn off 
+      rfEmitter.sendCode(sockets.f.off, function (error, stdout) {
+        if (!error) console.log(stdout);
+      });
+
+    }
+  });
+});
